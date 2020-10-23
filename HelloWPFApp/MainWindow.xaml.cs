26ebaf1d990df2bc51190f1e0cd590a8768fbe74
Lelen Abeywardena <lelabey@hotmail.com>
2020-10-23 12:28:41 -0400

    Added auto close Can remove from queue now

--- HelloWPFApp/MainWindow.xaml.cs
+++ HelloWPFApp/MainWindow.xaml.cs
@@ -51,81 +51,44 @@ namespace HelloWPFApp
             FileType.SelectedItem = "PNG";
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (AnimationButton.IsChecked == true)
             {
 
-                string strCmdText;
-                strCmdText = "-b \"" + ListBox1.SelectedItem.ToString() + "\" ";
-                if (CustomSettings.IsChecked == true)
-                {
-                    if (RenderPath.Text != "default")
-                    {
-                        strCmdText += "-o " + RenderPath.Text + " ";
-                    }
-                    strCmdText += "-F " + FileType.Text + " ";
-
-                    if (Int32.TryParse(FrameNumber.Text, out int frameNumber))
-                    {
-                        strCmdText += "-s " + FrameNumber.Text + " -e " + EndFrame.Text + " ";
-                    }
-                }
+                Render_Animation(ListBox1.SelectedItem.ToString());
                 
-                strCmdText += "-a";
-
-                Process process = new Process();
-                process.StartInfo = new ProcessStartInfo()
-                {
-                    WindowStyle = ProcessWindowStyle.Normal,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = false,
-                    CreateNoWindow = false,
-                    //StandardOutputEncoding = Encoding.UTF8,
-                    FileName = blenderDirectory.Text + "\\blender.exe",
-                    Arguments = strCmdText
-                };
-                process.EnableRaisingEvents = true;
-                process.Exited += new EventHandler(myProcess_Exited);
-                process.Start();
-                var responseString = await client.GetStringAsync("http://www.example.com/recepticle.aspx");
             }
             else if (StillButton.IsChecked == true)
             {
-                if (Int32.TryParse(FrameNumber.Text, out int frameNumber))
-                {
-                    string strCmdText;
-                    strCmdText = "-b \"" + ListBox1.SelectedItem.ToString() + "\" -F " + FileType.Text + " -f " + FrameNumber.Text;
-                    if (RenderPath.Text != "default")
-                    {
-                        strCmdText = "-b \"" + ListBox1.SelectedItem.ToString() + "\" -o " +RenderPath.Text+"\\ -F " + FileType.Text + " -f " + FrameNumber.Text;
-                    }
-                    
-                    Process process = new Process();
-                    process.StartInfo = new ProcessStartInfo()
-                    {
-                        WindowStyle = ProcessWindowStyle.Normal,
-                        UseShellExecute = false,
-                        RedirectStandardOutput = false,
-                        CreateNoWindow = false,
-                        //StandardOutputEncoding = Encoding.UTF8,
-                        FileName = blenderDirectory.Text + "\\blender.exe",
-                        Arguments = strCmdText
-                    };
 
-                    process.Start();
-                }
-                else {
-                    MessageBox.Show("Frame must be a number");
-                        } 
-                // Output: -105
-
+                Render_Still(ListBox1.SelectedItem.ToString());
 
             }
         }
 
-        private void myProcess_Exited(object sender, EventArgs e)
+        private async void myProcess_Exited(object sender, EventArgs e, String filename)
         {
+            DateTime now = DateTime.Now;
+            String timetext = now.Hour + ":" + now.Minute;
+            var values = new Dictionary<string, string>
+                {
+                    { "value1", filename },
+                    { "value2", timetext }
+                };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync("https://maker.ifttt.com/trigger/render_done/with/key/cYfoKvm6gdn7e49B2AptS0", content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            //var responseString = await client.GetStringAsync("https://maker.ifttt.com/trigger/render_done/with/key/cYfoKvm6gdn7e49B2AptS0");
+
+            
+            this.Dispatcher.Invoke(() =>
+            {
+                Render_Queue();
+            });
 
         }
 
@@ -172,6 +135,104 @@ namespace HelloWPFApp
             {
                 RenderPath.Text = ookiiDialog.SelectedPath;
             }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ListBox2.Items.Add(ListBox1.SelectedItem);
+        }
+
+        private void Queue_Start(object sender, RoutedEventArgs e)
+        {
+            if (ListBox2.HasItems == true)
+            {
+                Render_Queue();
+            }
+        }
+
+        private void Delete_From_Queue(object sender, RoutedEventArgs e)
+        {
+            if (ListBox2.HasItems == true)
+            {
+                ListBox2.Items.Remove(ListBox2.SelectedItem);
+            }
+        }
+
+        private void Render_Queue()
+        {
+            if (ListBox2.HasItems == true)
+            {
+                //Render_Still(ListBox2.Items.GetItemAt(0).ToString());
+                Render_Animation(ListBox2.Items.GetItemAt(0).ToString());
+                ListBox2.Items.Remove(ListBox2.Items.GetItemAt(0));
+            }
+            else
+            {
+                this.Close();
+            }
+
+        }
+
+        private void Render_Animation(String filename)
+        {
+            string strCmdText;
+            strCmdText = "-b \"" + filename + "\" ";
+            if (CustomSettings.IsChecked == true)
+            {
+                if (RenderPath.Text != "default")
+                {
+                    strCmdText += "-o " + RenderPath.Text + " ";
+                }
+                strCmdText += "-F " + FileType.Text + " ";
+
+                if (Int32.TryParse(FrameNumber.Text, out int frameNumber))
+                {
+                    strCmdText += "-s " + FrameNumber.Text + " -e " + EndFrame.Text + " ";
+                }
+            }
+
+            strCmdText += "-a";
+
+            Start_Process(strCmdText, filename);
+        }
+
+        private void Render_Still(String filename)
+        {
+            if (Int32.TryParse(FrameNumber.Text, out int frameNumber))
+            {
+                string strCmdText;
+                strCmdText = "-b \"" + filename + "\" -F " + FileType.Text + " -f " + FrameNumber.Text;
+                if (RenderPath.Text != "default")
+                {
+                    strCmdText = "-b \"" + ListBox1.SelectedItem.ToString() + "\" -o " + RenderPath.Text + "\\ -F " + FileType.Text + " -f " + FrameNumber.Text;
+                }
+
+                Start_Process(strCmdText, filename);
+                
+            }
+            else
+            {
+                MessageBox.Show("Frame must be a number");
+            }
+            // Output: -105
+        }
+
+        private void Start_Process(String strCmdText, String filename)
+        {
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo()
+            {
+                WindowStyle = ProcessWindowStyle.Normal,
+                UseShellExecute = false,
+                RedirectStandardOutput = false,
+                CreateNoWindow = false,
+                //StandardOutputEncoding = Encoding.UTF8,
+                FileName = blenderDirectory.Text + "\\blender.exe",
+                Arguments = strCmdText
+            };
+            process.EnableRaisingEvents = true;
+            process.Exited += new EventHandler((s, e) => myProcess_Exited(s, e, filename));
+            process.Start();
         }
     }
 }
